96dbed8eac26ae1beb9a2896db24898041c17abd
cn-90 <chr.revel@gmail.com>
2019-08-23 12:02:43 -0700

    Added stripe button to the checkout component, changed favicon & title

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import CheckoutItem from './../../components/checkout-item/Checkout-Item';
+
 import {
   selectCartItems,
   selectCartTotalPrice
 } from './../../redux/cart/cart.selectors';
 import './Checkout.scss';
+import StripeCheckoutButton from './../../components/stripe-button/Stripe-button';
 
 const checkoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -33,6 +35,12 @@ const checkoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
