c83f058c4a35527006b1c66ee0cb9e71b73e4ebb
cn-90 <chr.revel@gmail.com>
2019-08-27 08:57:41 -0700

    Moved Shop data from client to firebase, created With-Spinner HOC to deal with async loading of shop  data and implemented into the shop component

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,38 +3,74 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-	apiKey: 'AIzaSyCnatkJ_-gCuW9yZF31ZFXkdnYAF6aEalE',
-	authDomain: 'crwn-db-72513.firebaseapp.com',
-	databaseURL: 'https://crwn-db-72513.firebaseio.com',
-	projectId: 'crwn-db-72513',
-	storageBucket: '',
-	messagingSenderId: '455598155803',
-	appId: '1:455598155803:web:eeaa4a44ed9ccd05'
+  apiKey: 'AIzaSyCnatkJ_-gCuW9yZF31ZFXkdnYAF6aEalE',
+  authDomain: 'crwn-db-72513.firebaseapp.com',
+  databaseURL: 'https://crwn-db-72513.firebaseio.com',
+  projectId: 'crwn-db-72513',
+  storageBucket: '',
+  messagingSenderId: '455598155803',
+  appId: '1:455598155803:web:eeaa4a44ed9ccd05'
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-	if (!userAuth) return;
+  if (!userAuth) return;
 
-	const userRef = firestore.doc(`users/${userAuth.uid}`);
-	const snapShot = await userRef.get();
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
 
-	if (!snapShot.exists) {
-		const { displayName, email } = userAuth;
-		const createdAt = new Date();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-		try {
-			await userRef.set({
-				displayName,
-				email,
-				createdAt,
-				...additionalData
-			});
-		} catch (error) {
-			console.log('There was an error creating user.');
-		}
-	}
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('There was an error creating user.');
+    }
+  }
 
-	return userRef;
+  return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    console.log(newDocRef);
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  // takes collection items from firebase DB and modifies them into appropriate object.
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accum, collection) => {
+    accum[collection.title.toLowerCase()] = collection;
+    return accum;
+  }, {});
 };
 
 firebase.initializeApp(config);
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -2,16 +2,61 @@ import React from 'react';
 import CollectionsOverview from './../../components/collections-overview/Collections-overview';
 import { Route } from 'react-router-dom';
 import CollectionPage from './../collection/Collection';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+import WithSpinner from './../../components/with-spinner/With-spinner';
+import { connect } from 'react-redux';
+import { updateCollections } from './../../redux/shop/shop.actions';
 
-const Shop = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route
-      exact
-      path={`${match.path}/:collectionId`}
-      component={CollectionPage}
-    />
-  </div>
-);
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPagewWithSpinner = WithSpinner(CollectionPage);
 
-export default Shop;
+class Shop extends React.Component {
+  state = { loading: true };
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+    return (
+      <div className="shop-page">
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+        <Route
+          exact
+          path={`${match.path}/:collectionId`}
+          render={props => (
+            <CollectionPagewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Shop);
